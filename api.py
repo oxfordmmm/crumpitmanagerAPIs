c044b9e1d2ecba79fc1defd584c2f4d4b51ddb7a
Jez Swann <Jeremy.Swann@ndm.ox.ac.uk>
2019-04-05 16:40:34 +0100

    First functionality to get liveRunStats

---
+++ api.py
@@ -0,0 +1,59 @@
+#! /usr/bin/python3
+
+#RESTful API services for NanoporeManager
+#
+#Author: Jez Swann
+#Date: April 2019
+#Tutorial: http://blog.luisrei.com/articles/flaskrest.html
+
+import logging
+from flask import Flask, url_for, jsonify, request
+
+from runsInfo import *
+
+def setup_logging():
+    logger = logging.getLogger("api")
+    logger.setLevel(logging.DEBUG)
+    c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler("api.log")
+    c_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    f_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(f_handler)
+    logger.addHandler(c_handler)
+
+setup_logging()
+logger = logging.getLogger('api')
+logger.debug("Logging initialized")
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['GET'])
+def api_root():
+	rs = [1,'Welcome to Nanopore Manager APIs']	
+	return generateResponse(rs,200) 
+
+@app.route('/runs/getLiveStats',methods = ['GET'])
+def getLiveStats():
+	rs = [1, runsInfo().getLiveStats()]
+	return generateResponse(rs,200) 
+
+#input: result is an array
+#result[0] = 0 for OK -- else for error 
+#result[1] = data or error message
+#Status code list = http://www.flaskapi.org/api-guide/status-codes/
+def generateResponse(result, statusCode = None):
+	if statusCode is None:
+		if result[0] == 0:
+			statusCode = 200
+		else:
+			statusCode = 500
+
+	rs = {}
+	rs["status"] = result[0]	
+	rs["data"] = result[1]
+	resp = jsonify(rs)
+	resp.status_code = statusCode		
+	return resp
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5607)
---
+++ runsInfo.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import sys
+import os
+import pandas as pd
+import numpy as np
+import datetime
+from pymongo import MongoClient
+
+from runInfo import *
+
+class runsInfo:
+    def __init__(self,ip='127.0.0.1',port=27017):
+        self.client = MongoClient(ip, port)
+        self.loadtable()
+
+    def loadtable(self):
+        self.db = self.client['gridRuns']
+        self.collection = self.db.gridRuns
+        hce=self.collection.find()
+        log={}
+        for h in hce: log[h['run_name']]=h
+        
+        df=pd.DataFrame(log)
+        df=df.transpose()
+        self.df=df.sort_values(by=['starttime'],ascending=False)
+
+    def getLiveRuns(self, timeFrame: int=7):
+        if timeFrame > 0:
+            df=self.df[self.df.status == 'Running']
+            now=datetime.datetime.now()
+            td=now-datetime.timedelta(days=timeFrame)
+        return df[df.Submittedtime > td]
+
+    def getRuns(self):
+        return self.df
+
+    def getRunStatRows(self, r: pd.Series):
+        rs=runInfo(r.to_dict()).getLiveStats()
+        r['batches']=rs["batch_number"]
+        r['f5s']=rs["f5_numbers"]
+        r['percent complete']=rs["percent"]
+        return r
+
+    def getLiveStats(self):
+        liveRuns = self.getLiveRuns()
+        df = liveRuns.apply(self.getRunStatRows,axis=1)
+        try:
+            df=df[['Submittedtime','batches','f5s','percent complete','cwd']]
+        except:
+            print("Error: Could not find Live Stats")
+
+        return df.to_dict('index')
+
+if __name__ == '__main__':
+    print(runsInfo().getLiveStats())
