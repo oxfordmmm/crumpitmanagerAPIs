cfb4182d1940841777473606685c1a3114ea0abb
Jez Swann <Jeremy.Swann@ndm.ox.ac.uk>
2019-06-10 16:09:39 +0100

    Changed progress script to get infomation on the files on gridIONs

--- cronScripts/backupToCSProgress.bash
+++ cronScripts/backupToCSProgress.bash
@@ -8,8 +8,9 @@ function checkProgressOnRunStep()
     logFile=$3 
     findLogLoc=$4 
     outputDir=$5
-    gridLoc=$6
-    timeToDelete=$7
+    gridName=$6
+    gridLoc=$7
+    timeToDelete=$8
 
     find $runName/$step -type f -printf "%f\t%s\n" > ${findLogLoc}/${runName}-${step}-local.out
     if [ "$?" -ne "0" ]; then
@@ -20,7 +21,7 @@ function checkProgressOnRunStep()
             echo "$runName - $step - Has a directory but no files - (4)" | tee -a $logFile
         else
             cat ${findLogLoc}/${runName}-${step}-local.out | sort > ${findLogLoc}/${runName}-${step}-local.out.tmp && mv ${findLogLoc}/${runName}-${step}-local.out.tmp ${findLogLoc}/${runName}-${step}-local.out
-            ssh csana1 "cd ${outputDir}/Nanopore; find $runName/$step -type f -printf '%f\t%s\n'" > ${findLogLoc}/${runName}-${step}-remote.out
+            ssh csana1 "cd ${outputDir}; find $runName/$step -type f -printf '%f\t%s\n'" > ${findLogLoc}/${runName}-${step}-remote.out
             cat ${findLogLoc}/${runName}-${step}-remote.out | sort > ${findLogLoc}/${runName}-${step}-remote.out.tmp && mv ${findLogLoc}/${runName}-${step}-remote.out.tmp ${findLogLoc}/${runName}-${step}-remote.out
 
             outputx="$(diff ${findLogLoc}/${runName}-${step}-local.out ${findLogLoc}/${runName}-${step}-remote.out; echo x$?)"
@@ -32,10 +33,10 @@ function checkProgressOnRunStep()
                 findOlder=$(find $runName/$step -type f -mtime +7 -printf "%f\t%s\n"| wc -l)
                 if [ $findOlder -gt 0 ]; then
                     echo "$runName - $step - Local and Remote same - files older than 7 days, list for deletion - (1)" | tee -a $logFile
-                    echo "${gridLoc}/${runName}/${step}" >> ${logLoc}/toDelete
+                    echo "${gridName}/${runName}/${step}" >> ${logLoc}/toDelete
                 else
                     echo "$runName - $step - Local and Remote same - files newer than 7 days, not listing for deletion - (2)" | tee -a $logFile
-                    echo "${gridLoc}/${runName}/${step}" >> ${logLoc}/futureDelete
+                    echo "${gridName}/${runName}/${step}" >> ${logLoc}/futureDelete
                 fi
 
                 if [ "$step" != "sequencing_summary.txt.gz" ]; then
@@ -43,7 +44,15 @@ function checkProgressOnRunStep()
                     if [ "$findBatches" == "1" ]; then
                         findBatches=$(find $runName/$step -type f -printf "%f\n" \( -iname \*.fast5* -o -iname \*.fastq* \) | wc -l)
                     fi
-                    echo "{\"batches\" : $findBatches}" | tee -a $logFile
+
+                    findOriginalBatches="-1"
+                    if [ "$step" == "f5s" ]; then
+                        findOriginalBatches=$(find $gridLoc/$runName/$runName*/fast5_pass -type f -printf "%f\n" \( -iname \*.fast5* -o -iname \*.fastq* \) | wc -l)
+                    elif [ "$step" == "basecalled_fastq" ]; then
+                        findOriginalBatches=$(find $gridLoc/$runName/$runName/*/fastq_pass -type f -printf "%f\n" \( -iname \*.fast5* -o -iname \*.fastq* \) | wc -l)
+                    fi
+
+                    echo "{\"batches\" : $findBatches, \"gridBatches\" : $findOriginalBatches}" | tee -a $logFile
                 fi
             elif [ "$diffStatus" == "1" ]; then
                 echo "$runName - $step - Local and Remote different - (0)" | tee -a $logFile
@@ -52,10 +61,18 @@ function checkProgressOnRunStep()
                     if [ "$findBatches" == "1" ]; then
                         findBatches=$(find $runName/$step -type f -printf "%f\n" \( -iname \*.fast5* -o -iname \*.fastq* \) | wc -l)
                     fi
-                    echo "{\"batches\" : $findBatches}" | tee -a $logFile
+
+                    findOriginalBatches="-1"
+                    if [ "$step" == "f5s" ]; then
+                        findOriginalBatches=$(find $gridLoc/$runName/$runName/*/fast5_pass -type f -printf "%f\n" \( -iname \*.fast5* -o -iname \*.fastq* \) | wc -l)
+                    elif [ "$step" == "basecalled_fastq" ]; then
+                        findOriginalBatches=$(find $gridLoc/$runName/$runName/*/fastq_pass -type f -printf "%f\n" \( -iname \*.fast5* -o -iname \*.fastq* \) | wc -l)
+                    fi
+
+                    echo "{\"batches\" : $findBatches, \"gridBatches\" : $findOriginalBatches}" | tee -a $logFile
                 fi
                 echo "${output}" | tee -a $logFile
-#                echo "${gridLoc}/${runName}/${step}" >> ${logLoc}/toUpload
+#                echo "${gridName}/${runName}/${step}" >> ${logLoc}/toUpload
             else
                 echo "$runName - $step - Something happened - User Cancelled Script? - (-1)"  | tee -a $logFile
                 echo "${output}" | tee -a $logFile
@@ -65,12 +82,13 @@ function checkProgressOnRunStep()
     fi
 }
 
-grids=("grid0" "grid1" "grid2")
+gridLocs=("/mnt/grid0" "/mnt/grid1" "/mnt/grid2")
 steps=("f5s" "basecalled_fastq" "sequencing_summary.txt.gz")
 logTime="$(date +%F_%T)"
 logLoc="/var/log/CSbackups/progress/${logTime}"
 findLogLoc="${logLoc}/findLogs"
-outputDir="/mnt/microbio"
+crumpitOutLoc="/mnt/nanostore"
+outputDir="/mnt/microbio/Nanopore"
 timeToDelete="7"
 
 
@@ -81,11 +99,12 @@ touch ${logLoc}/futureDelete
 touch ${logLoc}/toUpload
 chmod -R a+r $logLoc
 
-for grid in "${grids[@]}"
+for gridLoc in "${gridLocs[@]}"
 do
-    logFile="${logLoc}/run-${grid}.log"
+    gridName=$(basename ${gridLoc})
+    logFile="${logLoc}/run-${gridName}.log"
     touch $logFile
-    cd /mnt/nanostore/${grid}
+    cd ${crumpitOutLoc}/${gridName}
     runs="$(ls -trd */)"
 
     for run in ${runs}
@@ -94,7 +113,7 @@ do
         echo ${runCut} | tee -a $logFile
         for step in ${steps[@]}
         do
-            checkProgressOnRunStep $runCut $step $logFile $findLogLoc $outputDir $grid $timeToDelete
+            checkProgressOnRunStep $runCut $step $logFile $findLogLoc $outputDir $gridName $gridLoc $timeToDelete
         done
         echo "-------------------------------------------------------------------------------" | tee -a $logFile
         echo "" | tee -a $logFile
